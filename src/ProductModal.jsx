6b4cb5e9123b61c144be11a270191311a18a49e8
Jesus Janeiro Montes <forek4st@Jesuss-MacBook-Pro.local>
2024-04-23 21:44:43 -0600

    se crea el componente all procuts

---
+++ src/ProductModal.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import "./AllProducts"; 
+
+const ProductModal = ({ closeModal, product }) => {
+  const closeModalButton = () => {
+    closeModal(); 
+  };
+
+  return ( 
+  
+  <div className="modal">
+  <div className="modal-content">
+    <span className="close" onClick={closeModalButton}>
+      &times;
+    </span>
+    <h2 className="modal-title">{product.name}</h2>
+    <div>carrousel</div>
+    <p className="modal-description">{product.description}</p>
+    <button className="btn-primary">Comprar</button>
+  </div>
+</div>
+);
+};
+
+
+ProductModal.propTypes = {
+  closeModal: PropTypes.func.isRequired,
+  product: PropTypes.object.isRequired, 
+};
+
+export default ProductModal;
