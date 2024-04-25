dce0e396f2c9272d2dbdca91155771d0d419d779
Forek4st <janeirojm91@gmail.com>
2024-04-25 17:46:57 -0600

    se modifica app.jsx y se agregan los imports corregidos de allproducts y se importa el componente login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import  { useState, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 const App = () => {
-  return (
-    <div>AQUI VA LA LANDING</div>
-  )
-}
+  const [userName, setUserName] = useState('');
 
-export default App
+  useEffect(() => { //OJO aqui puse un useEffect para que se modifique la renderizacion del componente y nos traiga el nombre de usuario  
+    
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token); 
+        if (decodedToken) {
+          const { userName } = decodedToken; 
+          setUserName(userName); 
+        }
+      } catch (error) {
+        console.error('Error al decodificar el token:', error);
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      {userName ? (
+        <div>Bienvenido {userName}</div>
+      ) : (
+        <div>AQUI VA LA LANDING</div>
+      )}
+    </div>
+  );
+};
+
+export default App;
