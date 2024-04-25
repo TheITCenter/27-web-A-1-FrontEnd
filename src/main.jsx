1aaf7e001bd6183587af39548141fc9da168f378
Forek4st <janeirojm91@gmail.com>
2024-04-24 20:57:03 -0600

    se agregan las rutas para la navegacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AllProducts from './AllProducts.jsx'
-import Header from './Pages/Header.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AllProducts from './AllProducts.jsx';
+import App from './App.jsx';
+import Gadgets from './Pages/Gadgets.jsx';
+import Header from './Pages/Header.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <AllProducts />
-  </React.StrictMode>,
-)
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/products/gadgets" element={<Gadgets />} />
+        <Route path="/products" element={<AllProducts />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
