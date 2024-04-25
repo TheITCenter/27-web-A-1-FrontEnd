9d1cc98f5574f9bf92b0bba04124f23d8699fffd
Forek4st <janeirojm91@gmail.com>
2024-04-25 17:48:35 -0600

    se crea el authcontex y authprovider

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { AuthContext } from "../Context/AuthContext.jsx";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { jwtDecode } from "jwt-decode";
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(token ? true : false);
+
+  const [user, setUser] = useState(token ? jwtDecode(token) : null);
+
+  const login = async (formData) => {
+    try {
+      const { data } = await axios.post(
+        "https://api404notfound.onrender.com/auth/login",
+        formData
+      );
+      localStorage.setItem("token", data.token);
+      if (data?.token) {
+        setToken(data.token);
+        setUser(jwtDecode(data.token));
+        setIsAuthenticated(true);
+        navigate("/");
+      }
+    } catch (error) {
+      const message = error.response.data.msg;
+      Swal.fire({
+        title: "Error!",
+        text: message,
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setIsAuthenticated(false);
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
