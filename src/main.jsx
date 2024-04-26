3f5fa3d516364d9b2a0a0a5a4355b098eaa035bb
Forek4st <janeirojm91@gmail.com>
2024-04-25 18:10:35 -0600

    se anaden las rutas de los componentes creados

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllProducts from '../src/Pages/AllProducts.jsx';
-import Login from '../src/Pages/Login.jsx';
-import App from './App.jsx';
-import Gadgets from './Pages/Gadgets.jsx';
-import Header from './Pages/Header.jsx';
-import { AuthProvider } from './Context/AuthProvider.jsx';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllProducts from "../src/Pages/AllProducts.jsx";
+import Gadgets from "./Pages/Gadgets.jsx";
+import Consolas from "./Pages/Consolas.jsx";
+import Monitors from "./Pages/Monitors.jsx";
+import Mobile from "./Pages/Mobile.jsx";
+import Tablets from "./Pages/Tablets.jsx";
+import PCs from "./Pages/PCs.jsx";
+import Login from "../src/Pages/Login.jsx";
+import Header from "./Pages/Header.jsx";
+import App from "./App.jsx";
+import { AuthProvider } from "./Context/AuthProvider.jsx";
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   
     <Router>
-    <AuthProvider> 
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route path="*" element={<App />} />
-        <Route path="/products/gadgets" element={<Gadgets />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route path="*" element={<App />} />
+          <Route path="/products/gadgets" element={<Gadgets />} />
+          <Route path="/products/consolas" element={<Consolas />} />
+          <Route path="/products/mobile" element={<Mobile />} />
+          <Route path="/products/monitors" element={<Monitors />} />
+          <Route path="/products/tablets" element={<Tablets />} />
+          <Route path="/products/pc" element={<PCs />} />
+
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </AuthProvider>
     </Router>
- 
   </React.StrictMode>
 );
