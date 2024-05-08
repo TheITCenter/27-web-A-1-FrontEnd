4af4ce00f6b425474d1570b0d2dc81d86ab80d3f
Forek4st <janeirojm91@gmail.com>
2024-05-07 20:11:55 -0600

    se arregla que se pueda tener productos negativos en el carrito

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -18,12 +18,12 @@ export const CartProvider = ({ children }) => {
       const existingProduct = prevCart.find((item) => item._id === product._id);
   
       if (existingProduct) {
-        // Incrementa la cantidad del producto existente
+      
         return prevCart.map((item) =>
           item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
         );
       } else {
-        // Agrega el nuevo producto al carrito con una cantidad inicial de 1
+     
         return [...prevCart, { ...product, quantity: 1 }];
       }
     });
@@ -41,7 +41,7 @@ export const CartProvider = ({ children }) => {
 
   const decreaseQuantity = (productId) => {
     setCart(cart.map(product => 
-      product._id === productId ? { ...product, quantity: product.quantity - 1 } : product
+      product._id === productId ? { ...product, quantity: product.quantity > 1 ? product.quantity - 1 : 1 } : product
     ));
   };
 
