46e4ad1adffff1f6d74891b87dcdc0510c76ff48
Forek4st <janeirojm91@gmail.com>
2024-05-07 18:29:56 -0600

    se usa el context para poder hacer las busquedas desde el input

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AllProducts from "../src/Pages/AllProducts.jsx";
 import Gadgets from "./Pages/Gadgets.jsx";
 import Consolas from "./Pages/Consolas.jsx";
@@ -13,40 +13,49 @@ import Header from "./Pages/Header.jsx";
 import App from "./App.jsx";
 import { AuthProvider } from "./Context/AuthProvider.jsx";
 import { CartProvider } from './Context/CartContext.jsx';
+import { SearchTermContext } from './Context/SearchContext.jsx'; 
 import "./index.css";
 import Footer from "./Pages/Footer.jsx";
 import Register from "./Pages/Register.jsx";
 import { HistorialCompras } from "./Pages/HistorialCompras.jsx";
 import { ToastContainer } from 'react-toastify';
 
+function AppWrapper() {
+  const [searchTerm, setSearchTerm] = useState('');
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-  <React.StrictMode>
-  <ToastContainer />
-<Router>
-  <AuthProvider>
-    <CartProvider>
-    <Header /> 
-       <Routes>
-         <Route exact path="/" element={<App />} />
-         <Route path="*" element={<App />} />
-         <Route path="/products/gadgets" element={<Gadgets />} />
-         <Route path="/products/consolas" element={<Consolas />} />
-         <Route path="/products/mobile" element={<Mobile />} />
-         <Route path="/products/monitors" element={<Monitors />} />
-         <Route path="/products/tablets" element={<Tablets />} />
-         <Route path="/products/pc" element={<PCs />} />
-         <Route path="/register" element={<Register />} />
-         <Route path="/products" element={<AllProducts />} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/myorders" element={<HistorialCompras />} />
-       </Routes>
-       <Footer /> 
-    </CartProvider>
-  </AuthProvider>
-</Router>
-</React.StrictMode>
-  </React.StrictMode>
-);
+  return (
+    <React.StrictMode>
+      <ToastContainer />
+      <Router>
+        <AuthProvider>
+          <CartProvider>
+            <SearchTermContext.Provider value={{ 
+              searchTerm, 
+              handleSearchChange: (event) => setSearchTerm(event.target.value) 
+            }}>
+              <Header />
+            
+            <Routes>
+              <Route exact path="/" element={<App />} />
+              <Route path="*" element={<App />} />
+              <Route path="/products/gadgets" element={<Gadgets />} />
+              <Route path="/products/consolas" element={<Consolas />} />
+              <Route path="/products/mobile" element={<Mobile />} />
+              <Route path="/products/monitors" element={<Monitors />} />
+              <Route path="/products/tablets" element={<Tablets />} />
+              <Route path="/products/pc" element={<PCs />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/products" element={<AllProducts />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/myorders" element={<HistorialCompras />} />
+            </Routes>
+            <Footer />
+             </SearchTermContext.Provider>
+          </CartProvider>
+        </AuthProvider>
+      </Router>
+    </React.StrictMode>
+  );
+}
 
+createRoot(document.getElementById("root")).render(<AppWrapper />);
